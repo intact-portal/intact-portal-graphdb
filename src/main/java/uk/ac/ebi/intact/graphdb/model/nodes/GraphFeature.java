092bc9953cd8b683743db7b7c0526800bd0f62cf
anjali <anjali@ebi.ac.uk>
2017-11-21 15:40:38 +0000

    NIV-49

--- src/main/java/uk/ac/ebi/intact/graphdb/model/nodes/GraphFeature.java
+++ src/main/java/uk/ac/ebi/intact/graphdb/model/nodes/GraphFeature.java
@@ -1,7 +1,282 @@
 package uk.ac.ebi.intact.graphdb.model.nodes;
 
+import org.neo4j.ogm.annotation.GraphId;
+import org.neo4j.ogm.annotation.NodeEntity;
+import psidev.psi.mi.jami.model.*;
+import psidev.psi.mi.jami.model.impl.AbstractFeature;
+import psidev.psi.mi.jami.model.impl.DefaultXref;
+import psidev.psi.mi.jami.utils.CvTermUtils;
+import psidev.psi.mi.jami.utils.XrefUtils;
+import psidev.psi.mi.xml.model.Feature;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+
 /**
  * Created by anjali on 21/11/17.
  */
-public class GraphFeature {
+@NodeEntity
+public class GraphFeature implements FeatureEvidence {
+
+    @GraphId
+    private Long id;
+
+    private Collection<CvTerm> detectionMethods;
+    private Collection<Parameter> parameters;
+    private String shortName;
+    private String fullName;
+    private Xref interpro;
+    private Collection<Xref> identifiers;
+    private Collection<Xref> xrefs;
+    private Collection<Annotation> annotations;
+    private CvTerm type;
+    private Collection<Range> ranges;
+    private Collection<Alias> aliases;
+
+    private CvTerm role;
+
+    private ParticipantEvidence participant;
+    private Collection<FeatureEvidence> linkedFeatures;
+
+    protected void initialiseParameters() {
+        this.parameters = new ArrayList<Parameter>();
+    }
+
+    protected void initialiseDetectionMethods(){
+        this.detectionMethods = new ArrayList<CvTerm>();
+    }
+
+    protected void initialiseDetectionMethodsWith(Collection<CvTerm> methods){
+        if (methods == null){
+            this.detectionMethods = Collections.EMPTY_LIST;
+        }
+        else {
+            this.detectionMethods = methods;
+        }
+    }
+
+    protected void initialiseParametersWith(Collection<Parameter> parameters) {
+        if (parameters == null){
+            this.parameters = Collections.EMPTY_LIST;
+        }
+        else {
+            this.parameters = parameters;
+        }
+    }
+
+    public Collection<CvTerm> getDetectionMethods() {
+        if (detectionMethods == null){
+            initialiseDetectionMethods();
+        }
+        return this.detectionMethods;
+    }
+
+    public Collection<Parameter> getParameters() {
+        if (parameters == null){
+            initialiseParameters();
+        }
+        return this.parameters;
+    }
+
+    protected void initialiseIdentifiers(){
+       // this.identifiers = new AbstractFeature.FeatureIdentifierList();
+    }
+
+    protected void initialiseAnnotations(){
+        this.annotations = new ArrayList<Annotation>();
+    }
+
+    protected void initialiseXrefs(){
+        this.xrefs = new ArrayList<Xref>();
+    }
+
+    protected void initialiseRanges(){
+        this.ranges = new ArrayList<Range>();
+    }
+
+    protected void initialiseIdentifiersWith(Collection<Xref> identifiers){
+        if (identifiers == null){
+            this.identifiers = Collections.EMPTY_LIST;
+        }
+        else {
+            this.identifiers = identifiers;
+        }
+    }
+
+    protected void initialiseAnnotationsWith(Collection<Annotation> annotations){
+        if (annotations == null){
+            this.annotations = Collections.EMPTY_LIST;
+        }
+        else {
+            this.annotations = annotations;
+        }
+    }
+
+    protected void initialiseXrefsWith(Collection<Xref> xrefs){
+        if (xrefs == null){
+            this.xrefs = Collections.EMPTY_LIST;
+        }
+        else {
+            this.xrefs = xrefs;
+        }
+    }
+
+    protected void initialiseRangesWith(Collection<Range> ranges){
+        if (ranges == null){
+            this.ranges = Collections.EMPTY_LIST;
+        }
+        else {
+            this.ranges = ranges;
+        }
+    }
+
+    protected void initialiseLinkedFeatures(){
+        this.linkedFeatures = new ArrayList<FeatureEvidence>();
+    }
+
+    protected void initialiseLinkedFeaturesWith(Collection<FeatureEvidence> features){
+        if (features == null){
+            this.linkedFeatures = Collections.EMPTY_LIST;
+        }
+        else {
+            this.linkedFeatures = features;
+        }
+    }
+
+    protected void initialiseAliases(){
+        this.aliases = new ArrayList<Alias>();
+    }
+
+    protected void initialiseAliasesWith(Collection<Alias> aliases){
+        if (aliases == null){
+            this.aliases = Collections.EMPTY_LIST;
+        }
+        else {
+            this.aliases = aliases;
+        }
+    }
+
+    public String getShortName() {
+        return this.shortName;
+    }
+
+    public void setShortName(String name) {
+        this.shortName = name;
+    }
+
+    public String getFullName() {
+        return this.fullName;
+    }
+
+    public void setFullName(String name) {
+        this.fullName = name;
+    }
+
+    public String getInterpro() {
+        return this.interpro != null ? this.interpro.getId() : null;
+    }
+
+    public void setInterpro(String interpro) {
+        Collection<Xref> featureIdentifiers = getIdentifiers();
+
+        // add new interpro if not null
+        if (interpro != null){
+            CvTerm interproDatabase = CvTermUtils.createInterproDatabase();
+            CvTerm identityQualifier = CvTermUtils.createIdentityQualifier();
+            // first remove old chebi if not null
+            if (this.interpro != null){
+                featureIdentifiers.remove(this.interpro);
+            }
+            this.interpro = new DefaultXref(interproDatabase, interpro, identityQualifier);
+            featureIdentifiers.add(this.interpro);
+        }
+        // remove all interpro if the collection is not empty
+        else if (!featureIdentifiers.isEmpty()) {
+            XrefUtils.removeAllXrefsWithDatabase(featureIdentifiers, Xref.INTERPRO_MI, Xref.INTERPRO);
+            this.interpro = null;
+        }
+    }
+
+    public Collection<Xref> getIdentifiers() {
+        if (identifiers == null){
+            initialiseIdentifiers();
+        }
+        return this.identifiers;
+    }
+
+    public Collection<Xref> getXrefs() {
+        if (xrefs == null){
+            initialiseXrefs();
+        }
+        return this.xrefs;
+    }
+
+    public Collection<Annotation> getAnnotations() {
+        if (annotations == null){
+            initialiseAnnotations();
+        }
+        return this.annotations;
+    }
+
+    public CvTerm getType() {
+        return this.type;
+    }
+
+    public void setType(CvTerm type) {
+        this.type = type;
+    }
+
+    public Collection<Range> getRanges() {
+        if (ranges == null){
+            initialiseRanges();
+        }
+        return this.ranges;
+    }
+
+    public CvTerm getRole() {
+        return this.role;
+    }
+
+    public void setRole(CvTerm effect) {
+        this.role = effect;
+    }
+
+    @Override
+    public ExperimentalEntity getParticipant() {
+        return null;
+    }
+
+    @Override
+    public void setParticipant(ExperimentalEntity participant) {
+
+    }
+
+    @Override
+    public void setParticipantAndAddFeature(ExperimentalEntity participant) {
+        if (this.participant != null){
+            this.participant.removeFeature(this);
+        }
+
+        if (participant != null){
+            participant.addFeature(this);
+        }
+    }
+
+
+    public Collection<FeatureEvidence> getLinkedFeatures() {
+        if(linkedFeatures == null){
+            initialiseLinkedFeatures();
+        }
+        return this.linkedFeatures;
+    }
+
+    public Collection<Alias> getAliases() {
+        if (this.aliases == null){
+            initialiseAliases();
+        }
+        return aliases;
+    }
+
+
 }
