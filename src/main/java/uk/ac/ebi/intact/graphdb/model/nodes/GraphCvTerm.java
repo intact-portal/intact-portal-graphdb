a01bca55cbdd025a6c84f484c0fb5d61c0f429ca
Noemi del Toro Ayllón <ntoro@ebi.ac.uk>
2017-11-21 11:11:02 +0000

    Adds GraphCvTerm

--- src/main/java/uk/ac/ebi/intact/graphdb/model/nodes/GraphCvTerm.java
+++ src/main/java/uk/ac/ebi/intact/graphdb/model/nodes/GraphCvTerm.java
@@ -3,8 +3,18 @@ package uk.ac.ebi.intact.graphdb.model.nodes;
 
 import org.neo4j.ogm.annotation.GraphId;
 import org.neo4j.ogm.annotation.NodeEntity;
+import psidev.psi.mi.jami.model.Alias;
+import psidev.psi.mi.jami.model.Annotation;
+import psidev.psi.mi.jami.model.CvTerm;
 import psidev.psi.mi.jami.model.Xref;
-import psidev.psi.mi.jami.model.impl.DefaultCvTerm;
+import psidev.psi.mi.jami.model.impl.DefaultXref;
+import psidev.psi.mi.jami.utils.CvTermUtils;
+import psidev.psi.mi.jami.utils.XrefUtils;
+import psidev.psi.mi.jami.utils.collection.AbstractListHavingProperties;
+import psidev.psi.mi.jami.utils.comparator.cv.UnambiguousCvTermComparator;
+
+import java.util.ArrayList;
+import java.util.Collection;
 
 /**
  * Created by IntelliJ IDEA.
@@ -13,29 +23,269 @@ import psidev.psi.mi.jami.model.impl.DefaultCvTerm;
  * Time: 08:40
  */
 @NodeEntity
-public class GraphCvTerm extends DefaultCvTerm {
+public class GraphCvTerm implements CvTerm {
 
     @GraphId
     private Long id;
 
+    private String shortName;
+    private String fullName;
+    private Collection<Xref> xrefs;
+    private Collection<Xref> identifiers;
+    private Collection<Annotation> annotations;
+    private Collection<Alias> synonyms;
 
-    public GraphCvTerm(String shortName) {
-        super(shortName);
+    private Xref miIdentifier;
+    private Xref modIdentifier;
+    private Xref parIdentifier;
+
+    public GraphCvTerm() {
     }
 
-    public GraphCvTerm(String shortName, String miIdentifier) {
-        super(shortName, miIdentifier);
+    public String getShortName() {
+        return shortName;
     }
 
-    public GraphCvTerm(String shortName, String fullName, String miIdentifier) {
-        super(shortName, fullName, miIdentifier);
+    public void setShortName(String name) {
+        if (name == null) {
+            throw new IllegalArgumentException("The short name cannot be null");
+        }
+        this.shortName = name;
     }
 
-    public GraphCvTerm(String shortName, Xref ontologyId) {
-        super(shortName, ontologyId);
+    public String getFullName() {
+        return this.fullName;
     }
 
-    public GraphCvTerm(String shortName, String fullName, Xref ontologyId) {
-        super(shortName, fullName, ontologyId);
+    public void setFullName(String name) {
+        this.fullName = name;
+    }
+
+
+    public Collection<Xref> getIdentifiers() {
+        if (identifiers == null) {
+            this.identifiers = new GraphCvTerm.CvTermIdentifierList();
+        }
+        return identifiers;
+    }
+
+    public String getMIIdentifier() {
+        return this.miIdentifier != null ? this.miIdentifier.getId() : null;
+    }
+
+    public void setMIIdentifier(String mi) {
+        Collection<Xref> cvTermIdentifiers = getIdentifiers();
+
+        // add new mi if not null
+        if (mi != null) {
+            CvTerm psiMiDatabase = CvTermUtils.createPsiMiDatabase();
+            CvTerm identityQualifier = CvTermUtils.createIdentityQualifier(psiMiDatabase);
+            // first remove old psi mi if not null
+            if (this.miIdentifier != null) {
+                cvTermIdentifiers.remove(this.miIdentifier);
+            }
+            this.miIdentifier = new DefaultXref(psiMiDatabase, mi, identityQualifier);
+            cvTermIdentifiers.add(this.miIdentifier);
+        }
+        // remove all mi if the collection is not empty
+        else if (!getIdentifiers().isEmpty()) {
+            XrefUtils.removeAllXrefsWithDatabase(getIdentifiers(), CvTerm.PSI_MI_MI, CvTerm.PSI_MI);
+            this.miIdentifier = null;
+        }
+    }
+
+    public String getMODIdentifier() {
+        return this.modIdentifier != null ? this.modIdentifier.getId() : null;
+    }
+
+    public void setMODIdentifier(String mod) {
+        Collection<Xref> cvTermIdentifiers = getIdentifiers();
+
+        // add new mod if not null
+        if (mod != null) {
+
+            CvTerm psiModDatabase = CvTermUtils.createPsiModDatabase();
+            CvTerm identityQualifier = CvTermUtils.createIdentityQualifier();
+            // first remove old psi mod if not null
+            if (this.modIdentifier != null) {
+                cvTermIdentifiers.remove(this.modIdentifier);
+            }
+            this.modIdentifier = new DefaultXref(psiModDatabase, mod, identityQualifier);
+            cvTermIdentifiers.add(this.modIdentifier);
+        }
+        // remove all mod if the collection is not empty
+        else if (!getIdentifiers().isEmpty()) {
+            XrefUtils.removeAllXrefsWithDatabase(getIdentifiers(), CvTerm.PSI_MOD_MI, CvTerm.PSI_MOD);
+            this.modIdentifier = null;
+        }
+    }
+
+    public String getPARIdentifier() {
+        return this.parIdentifier != null ? this.parIdentifier.getId() : null;
+    }
+
+    public void setPARIdentifier(String par) {
+        Collection<Xref> cvTermIdentifiers = getIdentifiers();
+
+        // add new mod if not null
+        if (par != null) {
+
+            CvTerm psiModDatabase = CvTermUtils.createPsiParDatabase();
+            CvTerm identityQualifier = CvTermUtils.createIdentityQualifier();
+            // first remove old psi mod if not null
+            if (this.parIdentifier != null) {
+                cvTermIdentifiers.remove(this.parIdentifier);
+            }
+            this.parIdentifier = new DefaultXref(psiModDatabase, par, identityQualifier);
+            cvTermIdentifiers.add(this.parIdentifier);
+        }
+        // remove all mod if the collection is not empty
+        else if (!getIdentifiers().isEmpty()) {
+            XrefUtils.removeAllXrefsWithDatabase(getIdentifiers(), null, CvTerm.PSI_PAR);
+            this.parIdentifier = null;
+        }
+    }
+
+    public Collection<Xref> getXrefs() {
+        if (xrefs == null) {
+            this.xrefs = new ArrayList<Xref>();
+        }
+        return this.xrefs;
+    }
+
+    public Collection<Annotation> getAnnotations() {
+        if (annotations == null) {
+            this.annotations = new ArrayList<Annotation>();
+        }
+        return this.annotations;
+    }
+
+    public Collection<Alias> getSynonyms() {
+        if (synonyms == null) {
+            this.synonyms = new ArrayList<Alias>();
+        }
+        return this.synonyms;
+    }
+
+    protected void processAddedIdentifierEvent(Xref added) {
+
+        // the added identifier is psi-mi and it is not the current mi identifier
+        if (miIdentifier != added && XrefUtils.isXrefFromDatabase(added, CvTerm.PSI_MI_MI, CvTerm.PSI_MI)) {
+            // the current psi-mi identifier is not identity, we may want to set miIdentifier
+            if (!XrefUtils.doesXrefHaveQualifier(miIdentifier, Xref.IDENTITY_MI, Xref.IDENTITY)) {
+                // the miidentifier is not set, we can set the miidentifier
+                if (miIdentifier == null) {
+                    miIdentifier = added;
+                } else if (XrefUtils.doesXrefHaveQualifier(added, Xref.IDENTITY_MI, Xref.IDENTITY)) {
+                    miIdentifier = added;
+                }
+                // the added xref is secondary object and the current mi is not a secondary object, we reset miidentifier
+                else if (!XrefUtils.doesXrefHaveQualifier(miIdentifier, Xref.SECONDARY_MI, Xref.SECONDARY)
+                        && XrefUtils.doesXrefHaveQualifier(added, Xref.SECONDARY_MI, Xref.SECONDARY)) {
+                    miIdentifier = added;
+                }
+            }
+        }
+        // the added identifier is psi-mod and it is not the current mod identifier
+        else if (modIdentifier != added && XrefUtils.isXrefFromDatabase(added, CvTerm.PSI_MOD_MI, CvTerm.PSI_MOD)) {
+            // the current psi-mod identifier is not identity, we may want to set modIdentifier
+            if (!XrefUtils.doesXrefHaveQualifier(modIdentifier, Xref.IDENTITY_MI, Xref.IDENTITY)) {
+                // the modIdentifier is not set, we can set the modIdentifier
+                if (modIdentifier == null) {
+                    modIdentifier = added;
+                } else if (XrefUtils.doesXrefHaveQualifier(added, Xref.IDENTITY_MI, Xref.IDENTITY)) {
+                    modIdentifier = added;
+                }
+                // the added xref is secondary object and the current mi is not a secondary object, we reset miidentifier
+                else if (!XrefUtils.doesXrefHaveQualifier(modIdentifier, Xref.SECONDARY_MI, Xref.SECONDARY)
+                        && XrefUtils.doesXrefHaveQualifier(added, Xref.SECONDARY_MI, Xref.SECONDARY)) {
+                    modIdentifier = added;
+                }
+            }
+        }
+        // the added identifier is psi-par and it is not the current par identifier
+        else if (parIdentifier != added && XrefUtils.isXrefFromDatabase(added, null, CvTerm.PSI_PAR)) {
+            // the current psi-par identifier is not identity, we may want to set parIdentifier
+            if (!XrefUtils.doesXrefHaveQualifier(parIdentifier, Xref.IDENTITY_MI, Xref.IDENTITY)) {
+                // the parIdentifier is not set, we can set the parIdentifier
+                if (parIdentifier == null) {
+                    parIdentifier = added;
+                } else if (XrefUtils.doesXrefHaveQualifier(added, Xref.IDENTITY_MI, Xref.IDENTITY)) {
+                    parIdentifier = added;
+                }
+                // the added xref is secondary object and the current par is not a secondary object, we reset paridentifier
+                else if (!XrefUtils.doesXrefHaveQualifier(parIdentifier, Xref.SECONDARY_MI, Xref.SECONDARY)
+                        && XrefUtils.doesXrefHaveQualifier(added, Xref.SECONDARY_MI, Xref.SECONDARY)) {
+                    parIdentifier = added;
+                }
+            }
+        }
+    }
+
+    protected void processRemovedIdentifierEvent(Xref removed) {
+        // the removed identifier is psi-mi
+        if (miIdentifier != null && miIdentifier.equals(removed)) {
+            miIdentifier = XrefUtils.collectFirstIdentifierWithDatabase(getIdentifiers(), CvTerm.PSI_MI_MI, CvTerm.PSI_MI);
+        }
+        // the removed identifier is psi-mod
+        else if (modIdentifier != null && modIdentifier.equals(removed)) {
+            modIdentifier = XrefUtils.collectFirstIdentifierWithDatabase(getIdentifiers(), CvTerm.PSI_MOD_MI, CvTerm.PSI_MOD);
+        }
+        // the removed identifier is psi-par
+        else if (parIdentifier != null && parIdentifier.equals(removed)) {
+            parIdentifier = XrefUtils.collectFirstIdentifierWithDatabase(getIdentifiers(), null, CvTerm.PSI_PAR);
+        }
+    }
+
+    protected void clearPropertiesLinkedToIdentifiers() {
+        miIdentifier = null;
+        modIdentifier = null;
+        parIdentifier = null;
+    }
+
+    @Override
+    public int hashCode() {
+        return UnambiguousCvTermComparator.hashCode(this);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+
+        if (!(o instanceof CvTerm)) {
+            return false;
+        }
+
+        return UnambiguousCvTermComparator.areEquals(this, (CvTerm) o);
+    }
+
+    @Override
+    public String toString() {
+        return (getMIIdentifier() != null ? getMIIdentifier() :
+                (getMODIdentifier() != null ? getMODIdentifier() :
+                (getPARIdentifier() != null ? getPARIdentifier() : "-"))) + " (" + getShortName() + ")";
+    }
+
+    private class CvTermIdentifierList extends AbstractListHavingProperties<Xref> {
+        public CvTermIdentifierList() {
+            super();
+        }
+
+        @Override
+        protected void processAddedObjectEvent(Xref added) {
+            processAddedIdentifierEvent(added);
+        }
+
+        @Override
+        protected void processRemovedObjectEvent(Xref removed) {
+            processRemovedIdentifierEvent(removed);
+        }
+
+        @Override
+        protected void clearProperties() {
+            clearPropertiesLinkedToIdentifiers();
+        }
     }
 }
