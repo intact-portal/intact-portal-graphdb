a6d3045b3af2ac7cf852173af1bb0ea66a316e68
anjali <anjali@ebi.ac.uk>
2017-11-24 10:38:53 +0000

    NIV-48

---
+++ src/main/java/uk/ac/ebi/intact/graphdb/model/nodes/GraphInteractionEvidence.java
@@ -0,0 +1,7 @@
+package uk.ac.ebi.intact.graphdb.model.nodes;
+
+/**
+ * Created by anjali on 23/11/17.
+ */
+public class GraphInteractionEvidence {
+}
---
+++ src/main/java/uk/ac/ebi/intact/graphdb/model/nodes/GraphVariableParameterValue.java
@@ -0,0 +1,7 @@
+package uk.ac.ebi.intact.graphdb.model.nodes;
+
+/**
+ * Created by anjali on 24/11/17.
+ */
+public class GraphVariableParameterValue {
+}
