bbe8e1416f3a9a6db2081fe9440ed5cf357a2d94
anjali <anjali@ebi.ac.uk>
2018-03-06 15:29:33 +0000

    NIV-78

--- src/main/java/uk/ac/ebi/intact/graphdb/utils/CypherQueries.java
+++ src/main/java/uk/ac/ebi/intact/graphdb/utils/CypherQueries.java
@@ -6,5 +6,9 @@ package uk.ac.ebi.intact.graphdb.utils;
 public class CypherQueries {
 
     /*Common BinaryInteractionEvidences of any two interactors*/
-    public static final String COMM_NEIGH_OF_INTOR="MATCH (interactorA:GraphInteractor)--(binaryIE:GraphBinaryInteractionEvidence)--(interactorB:GraphInteractor) WHERE NOT (interactorA) = (interactorB) RETURN interactorA,COLLECT(binaryIE) as binaryInteractionEvidences,interactorB";
+ //   public static final String COMM_NEIGH_OF_INTOR="MATCH (interactorA:GraphInteractor)--(binaryIE:GraphBinaryInteractionEvidence)--(interactorB:GraphInteractor) WHERE NOT (interactorA) = (interactorB) RETURN interactorA,COLLECT(binaryIE) as binaryInteractionEvidences,interactorB";
+    public static final String COMM_NEIGH_OF_INTOR=  "MATCH (interactorA:GraphInteractor)--(binaryIE:GraphInteractionEvidence)--(interactorB:GraphInteractor) \n" +
+            "MATCH (binaryIE:GraphInteractionEvidence) -[experiment] -(graphExperiment:GraphExperiment)-[interactionDetectionMethod:INTERACTION_DETECTION_METHOD]-(dm_cvterm:GraphCvTerm)\n" +
+            "MATCH (binaryIE:GraphInteractionEvidence) - [interactionType:INTERACTION_TYPE] - (it_cvterm:GraphCvTerm)\n" +
+            "WHERE NOT (interactorA) = (interactorB) RETURN interactorA,COLLECT(binaryIE) as binaryInteractionEvidences,interactorB,experiment,graphExperiment,interactionDetectionMethod,dm_cvterm,interactionType,it_cvterm";
 }
