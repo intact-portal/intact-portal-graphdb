e7eef3e3eb3f769a358753b02d1c80195bd667bd
anjali <anjali@ebi.ac.uk>
2019-05-14 14:39:07 +0100

    NIV-275 - Creation of Interaction evidence node

--- src/main/java/uk/ac/ebi/intact/graphdb/model/nodes/GraphInteractionEvidence.java
+++ src/main/java/uk/ac/ebi/intact/graphdb/model/nodes/GraphInteractionEvidence.java
@@ -118,7 +118,7 @@ public class GraphInteractionEvidence implements InteractionEvidence {
         }
 
         if (CreationConfig.createNatively) {
-            if (!isAlreadyCreated() && !childAlreadyCreated) {
+            if (!childAlreadyCreated) {
                 createRelationShipNatively(this.getGraphId());
             }
         }
