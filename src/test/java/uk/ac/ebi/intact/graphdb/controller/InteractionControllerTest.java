364b18d654bf3044e76a477f1b89deecc67d5371
ntoro <ntoro@ebi.ac.uk>
2019-05-29 12:24:25 +0100

    NIV-284: Automatise the port for the controller testing.

--- src/test/java/uk/ac/ebi/intact/graphdb/controller/InteractionControllerTest.java
+++ src/test/java/uk/ac/ebi/intact/graphdb/controller/InteractionControllerTest.java
@@ -1,36 +1,29 @@
 package uk.ac.ebi.intact.graphdb.controller;
 
 
-import org.apache.http.HttpEntity;
-import org.apache.http.HttpResponse;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.client.methods.HttpUriRequest;
-import org.apache.http.entity.ContentType;
-import org.apache.http.impl.client.HttpClientBuilder;
-import org.apache.http.util.EntityUtils;
 import org.json.JSONException;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
-import org.junit.Assert;
-import org.junit.Assume;
-import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.skyscreamer.jsonassert.JSONAssert;
 import org.skyscreamer.jsonassert.JSONCompareMode;
-import org.springframework.beans.factory.annotation.Value;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.io.FileReader;
 import java.io.IOException;
-import java.net.ConnectException;
-import java.net.HttpURLConnection;
-import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 
 /**
  * Created by anjali on 26/03/19.
@@ -40,62 +33,47 @@ import java.util.List;
 @Transactional
 public class InteractionControllerTest {
 
-    @Value("${graph.db.rest.service.url}")
-    private String graphDBServerUrl;
+    @LocalServerPort
+    private int port;
 
-    @Before
-    public void checkIfServerIsUp() throws Exception {
-        boolean condition = true;
-        try {
+    @Autowired
+    private InteractionController controller;
 
-            URL siteURL = new URL(graphDBServerUrl);
-            HttpURLConnection connection = (HttpURLConnection) siteURL.openConnection();
-            connection.setRequestMethod("GET");
-            connection.setConnectTimeout(3000);
-            connection.connect();
-            int code = connection.getResponseCode();
-            Assert.assertEquals("Graph DB Web Service response code is incorrect", 200, code);
-        } catch (ConnectException e) {
-            System.err.println("Graph DB Web Service Seems to be Down, please start it");
-            condition = false;
-        } catch (Exception e) {
-            System.err.println("Exception while connecting to graph db server");
-            condition = false;
-        }
-        Assume.assumeTrue(condition);
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @Test
+    public void contexLoads() {
+        assertNotNull(controller);
     }
 
     @Test
     public void exportInteraction() {
 
         List<String> interactionAcsToTest = new ArrayList<>();
-        interactionAcsToTest.add("EBI-10052707"); // generic checking
-        interactionAcsToTest.add("EBI-10048599");// for checking Interaction Parameters
+        interactionAcsToTest.add("EBI-10052707");// generic checking
+        interactionAcsToTest.add("EBI-10048599");// for checking interaction parameters
         interactionAcsToTest.add("EBI-10049314");// for checking expressedIn
         interactionAcsToTest.add("EBI-10054743");// for checking linkedFeatures
         interactionAcsToTest.add("EBI-10042058");// for checking linkedFeatures
-        interactionAcsToTest.add("EBI-1005174");// for checking interpro
-        interactionAcsToTest.add("EBI-1004945");// for checking experiment Modifications
+        interactionAcsToTest.add("EBI-1005174"); // for checking interpro
+        interactionAcsToTest.add("EBI-1004945"); // for checking experiment modifications
+
         for (String interactionAc : interactionAcsToTest) {
             try {
 
-                System.out.println("Unit Test for Interaction with ac: " + interactionAc);
-                String jsonMimeType = "application/json";
-                HttpUriRequest request = new HttpGet(graphDBServerUrl + "/graph/interaction/export/" + interactionAc);
-                // deleted participant and feature ids as it was hindering with order ignoring while json comparison
+                System.out.println("Test interaction with ac: " + interactionAc);
 
-                HttpResponse response = HttpClientBuilder.create().build().execute(request);
+                ResponseEntity<String> response = restTemplate.getForEntity("http://localhost:" + port +
+                        "/graph/interaction/export/" + interactionAc, String.class);
 
-                Assert.assertNotNull("Response is null", response);
-                Assert.assertNotNull("Response status line is null", response.getStatusLine());
-                Assert.assertEquals("Response Code is wrong", 200, response.getStatusLine().getStatusCode());
+                assertNotNull("Response is null", response);
+                assertNotNull("Response status line is null", response.getStatusCode());
+                assertEquals("Response Code is wrong", 200, response.getStatusCodeValue());
 
-                HttpEntity httpEntity = response.getEntity();
-                String mimeType = ContentType.getOrDefault(httpEntity).getMimeType();
-
-                Assert.assertEquals(jsonMimeType, mimeType);
-
-                String actualJsonString = EntityUtils.toString(httpEntity);
+                assertNotNull(response.getHeaders().getContentType());
+                assertEquals("application", response.getHeaders().getContentType().getType());
+                assertEquals("json", response.getHeaders().getContentType().getSubtype());
 
                 JSONParser parser = new JSONParser();
                 //Use JSONObject for simple JSON and JSONArray for array of JSON.
@@ -104,7 +82,7 @@ public class InteractionControllerTest {
                 String expectedJsonString = data.toJSONString();
 
                 JSONAssert.assertEquals(
-                        expectedJsonString, actualJsonString, JSONCompareMode.LENIENT);
+                        expectedJsonString, response.getBody(), JSONCompareMode.LENIENT);
 
                 // use below if you need to ignore fields
             /*JSONAssert.assertEquals(expectedJsonString, actualJsonString,
@@ -123,13 +101,12 @@ public class InteractionControllerTest {
                         arrayValueMatchCustomization);
                 JSONAssert.assertEquals(expectedJsonString, actualJsonString, customArrayValueComparator);*/
 
-
             } catch (IOException e) {
                 System.err.println("Exception while connecting to Graph DB Web Service");
                 e.printStackTrace();
             } catch (JSONException jsonException) {
                 jsonException.printStackTrace();
-                System.err.println("Exception while compating json");
+                System.err.println("Exception while comparing json");
             } catch (ParseException parseException) {
                 parseException.printStackTrace();
                 System.err.println("Exception while parsing expectedJsonFile");
