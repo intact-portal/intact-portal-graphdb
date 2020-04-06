1f27524f04b75551105c1a42f31791912ac12653
anjali <anjali@ebi.ac.uk>
2020-04-06 14:35:38 +0100

    CYTA-37: Unit Tests written for extracting nodes for cytoscape app

--- intact-graphdb-service/src/test/java/uk/ac/ebi/intact/graphdb/repository/GraphBinaryInteractionEvidenceRepositoryTest.java
+++ intact-graphdb-service/src/test/java/uk/ac/ebi/intact/graphdb/repository/GraphBinaryInteractionEvidenceRepositoryTest.java
@@ -14,6 +14,7 @@ import org.springframework.transaction.annotation.Transactional;
 import uk.ac.ebi.intact.graphdb.model.nodes.GraphBinaryInteractionEvidence;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 
@@ -61,13 +62,82 @@ public class GraphBinaryInteractionEvidenceRepositoryTest {
         species.add(9606);
 
         Iterable<Map<String, Object>> nodesIterable1 = graphBinaryInteractionEvidenceRepository.findCyAppNodes(identifiers, null);
-        Assert.assertEquals(152, Iterables.count(nodesIterable1));//152
+        Assert.assertEquals(8, Iterables.count(nodesIterable1));//152
+
+        boolean interactorsPresent1 = false;
+        Iterator<Map<String, Object>> iterator1 = nodesIterable1.iterator();
+        try {
+            while (iterator1.hasNext()) {
+                Map<String, Object> map = iterator1.next();
+                for (Map identifierMap : (Map[]) map.get("xrefs")) {
+                    if (identifierMap.get("xref_id").equals("Q9BZD4") || identifierMap.get("xref_id").equals("O14777")) {
+                        interactorsPresent1 = true;
+                    }
+                }
+            }
+        } catch (Exception e) {
+            Assert.assertTrue("A map with the key value was expected", false);
+        }
+
+        Assert.assertTrue("Queried Interactors should have been present", interactorsPresent1);
 
         Iterable<Map<String, Object>> nodesIterable2 = graphBinaryInteractionEvidenceRepository.findCyAppNodes(identifiers, species);
-        Assert.assertEquals(141, Iterables.count(nodesIterable2));//141
+        Assert.assertEquals(8, Iterables.count(nodesIterable2));//141
+
+        Map<String, Object> mapToBeTested = null;
+        boolean interactorsPresent = false;
+        Iterator<Map<String, Object>> iterator2 = nodesIterable2.iterator();
+        try {
+            while (iterator2.hasNext()) {
+                Map<String, Object> map = iterator2.next();
+                if (map.get("id").equals("EBI-949451")) {
+                    mapToBeTested = map;
+                }
+
+                if (!map.get("species").equals("Homo sapiens")) {
+                    Assert.assertTrue("Only Human species records were expected", false);
+                }
+
+                for (Map identifierMap : (Map[]) map.get("xrefs")) {
+                    if (identifierMap.get("xref_id").equals("Q9BZD4") || identifierMap.get("xref_id").equals("O14777")) {
+                        interactorsPresent = true;
+                    }
+                }
+            }
+        } catch (Exception e) {
+            Assert.assertTrue("A map with the key value was expected", false);
+        }
+
+        if (mapToBeTested == null) {
+            Assert.assertTrue("An interactor with id 'EBI-949451' was expected", false);
+        }
+        Assert.assertTrue("Queried Interactors should have been present", interactorsPresent);
+        Assert.assertTrue(mapToBeTested.get("preferred_id").equals("P07199"));
+        Assert.assertTrue(mapToBeTested.get("species").equals("Homo sapiens"));
+        Assert.assertTrue(mapToBeTested.get("taxId").equals(9606));
+        Assert.assertTrue(mapToBeTested.get("label").equals("CENPB(P07199)"));
+        Assert.assertTrue(mapToBeTested.get("type").equals("protein"));
+        Assert.assertTrue(mapToBeTested.get("type_mi_dentifier").equals("MI:0326"));
+        Assert.assertTrue(mapToBeTested.get("type_mod_identifier") == null);
+        Assert.assertTrue(mapToBeTested.get("type_par_identifier") == null);
+        Assert.assertTrue(mapToBeTested.get("interactor_name").equals("CENPB"));
+        Assert.assertEquals(3, ((Map[]) mapToBeTested.get("xrefs")).length);
 
         Iterable<Map<String, Object>> nodesIterable3 = graphBinaryInteractionEvidenceRepository.findCyAppNodes(null, species);
-        Assert.assertEquals(30179, Iterables.count(nodesIterable3));// 30179
+        Assert.assertEquals(473, Iterables.count(nodesIterable3));// 30179
+
+        Iterator<Map<String, Object>> iterator3 = nodesIterable3.iterator();
+        try {
+            while (iterator3.hasNext()) {
+                Map<String, Object> map = iterator3.next();
+
+                if (!map.get("species").equals("Homo sapiens")) {
+                    Assert.assertTrue("Only Human species records were expected", false);
+                }
+            }
+        } catch (Exception e) {
+            Assert.assertTrue("A map with the key value was expected", false);
+        }
 
         /*Iterable<Map<String, Object>> nodesIterable2 = graphBinaryInteractionEvidenceRepository.findBinaryInteractionsForCyAppNodes(null);
         Assert.assertEquals(112316, Iterables.count(nodesIterable2));*/
