f8fdd759e2cb4fd799e8882645eedbd63fcef0c3
anjali <anjali@ebi.ac.uk>
2020-05-01 14:20:27 +0100

    CYTA-53,CYTA-52: Cytoscape small fixes

--- intact-graphdb-service/src/main/java/uk/ac/ebi/intact/graphdb/utils/NetworkNodeParamNames.java
+++ intact-graphdb-service/src/main/java/uk/ac/ebi/intact/graphdb/utils/NetworkNodeParamNames.java
@@ -7,11 +7,13 @@ public class NetworkNodeParamNames {
 
     public static final String ID = "id";
     public static final String PREFERRED_ID = "preferred_id";
+    public static final String PREFERRED_ID_DB_NAME = "preferred_id_database_name";
+    public static final String PREFERRED_ID_DB_MI = "preferred_id_database_mi_identifier";
     public static final String SPECIES = "species";
     public static final String TAXID = "taxId";
     public static final String LABEL = "label";
     public static final String TYPE = "type";
-    public static final String TYPE_MI_IDENTIFIER = "type_mi_dentifier";
+    public static final String TYPE_MI_IDENTIFIER = "type_mi_identifier";
     public static final String TYPE_MOD_IDENTIFIER = "type_mod_identifier";
     public static final String TYPE_PAR_IDENTIFIER = "type_par_identifier";
     public static final String INTERACTOR_NAME = "interactor_name";
