77c7c6f545a88f0446c8329a9af94e38171abf03
anjali <anjali@ebi.ac.uk>
2020-07-07 11:41:47 +0100

    Added more fields for cytoscape app

--- intact-graphdb-service/src/main/java/uk/ac/ebi/intact/graphdb/utils/NetworkEdgeParamNames.java
+++ intact-graphdb-service/src/main/java/uk/ac/ebi/intact/graphdb/utils/NetworkEdgeParamNames.java
@@ -13,6 +13,8 @@ public class NetworkEdgeParamNames {
     public static final String INTERACTION_TYPE_MI_IDENTIFIER = "interaction_type_mi_identifier";
     public static final String INTERACTION_DETECTION_METHOD = "interaction_detection_method";
     public static final String INTERACTION_DETECTION_METHOD_MI_IDENTIFIER = "interaction_detection_method_mi_identifier";
+    public static final String PARTICIPANT_DETECTION_METHOD = "participant_detection_method";
+    public static final String PARTICIPANT_DETECTION_METHOD_MI_IDENTIFIER = "participant_detection_method_mi_identifier";
     public static final String MI_SCORE = "mi_score";
     public static final String HOST_ORGANISM = "host_organism";
     public static final String HOST_ORGANISM_TAX_ID = "host_organism_tax_id";
@@ -20,6 +22,8 @@ public class NetworkEdgeParamNames {
     public static final String EXPANSION_TYPE = "expansion_type";
     public static final String PARTICIPANT_BIOLOGICAL_ROLE = "participant_biological_role_name";
     public static final String PARTICIPANT_BIOLOGICAL_ROLE_MI_IDENTIFIER = "participant_biological_role_mi_identifier";
+    public static final String PARTICIPANT_EXPERIMENTAL_ROLE = "participant_experimental_role_name";
+    public static final String PARTICIPANT_EXPERIMENTAL_ROLE_MI_IDENTIFIER = "participant_experimental_role_mi_identifier";
     public static final String PARTICIPANT_FEATURES = "participant_features";
     public static final String ANNOTATIONS = "annotations";
     public static final String PARAMS = "parameters";
